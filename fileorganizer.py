7b7b9b225bc79499ecb1eb22555e2b4aec2ad03e
Shishir-Ashok <shishirashok17@gmail.com>
2019-09-16 02:14:30 +0530

    Few changes

--- fileorganizer.py
+++ fileorganizer.py
@@ -1,9 +1,8 @@
 from watchdog.observers import Observer
-import time
+from watchdog.events import FileSystemEventHandler
 import os
+import time
 import json
-from watchdog.events import PatternMatchingEventHandler
-
 
 
 folder_to_track = '/Users/shishir/Desktop/folder' 
@@ -21,50 +20,45 @@ zip_folder = '/Users/shishir/Desktop/folder/Zip files'
 others_folder = '/Users/shishir/Desktop/folder/Others'
 
 
-patterns = "*"
-ignore_patterns = ""
-ignore_directories = True
-case_sensitive = True
-event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
-
-def on_change(event):
-	for file in os.listdir(folder_to_track):
-		if os.path.isfile(folder_to_track+ "/" + file):
-			print(file)
-			name, ext = os.path.splitext(file)
-			if (ext == '.html'):
-				src = folder_to_track + "/" + file
-				new_destination = html_folder + "/" + file
-				os.rename(src,new_destination)
-			elif (ext == '.txt'):
-				src = folder_to_track + "/" + file
-				new_destination = documents_folder + "/" + file
-				os.rename(src,new_destination)
-			elif (ext == '.ai'):
-				src = folder_to_track + "/" + file
-				new_destination = ai_folder + "/" + file
-				os.rename(src,new_destination)
-			else:
-				src = folder_to_track + "/" + file
-				new_destination = others_folder + "/" + file
-				os.rename(src,new_destination)
-
-
-event_handler.on_modified = on_change
-event_handler.on_moved = on_change
-event_handler.on_created = on_change
 
 
 
-go_recursively = True
+
+class MyHandler(FileSystemEventHandler):
+	i =1
+	# print("inside class\n")
+	def on_modified(self,event):
+		# print("inside self")
+		for file in os.listdir(folder_to_track):
+			if os.path.isfile(folder_to_track+ "/" + file):
+				name, ext = os.path.splitext(file)
+				if (ext == '.html'):
+					src = folder_to_track + "/" + file
+					new_destination = html_folder + "/" + file
+					os.rename(src,new_destination)
+				elif (ext == '.txt'):
+					src = folder_to_track + "/" + file
+					new_destination = documents_folder + "/" + file
+					os.rename(src,new_destination)
+				elif (ext == '.ai'):
+					src = folder_to_track + "/" + file
+					new_destination = ai_folder + "/" + file
+					os.rename(src,new_destination)
+				else:
+					src = folder_to_track + "/" + file
+					new_destination = others_folder + "/" + file
+					os.rename(src,new_destination)
+
+
+
+event_handler = MyHandler()
 observer = Observer()
-observer.schedule(event_handler, folder_to_track, recursive=go_recursively)
+observer.schedule(event_handler,folder_to_track,recursive=True)
 observer.start()
-
 
 try:
 	while True:
-		time.sleep(10)
+		time.sleep(1)
 except KeyboardInterrupt:
 	observer.stop()
 observer.join()
